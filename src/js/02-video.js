45b5c304620b5bd0103b7e2485b826dd679640de
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-08-11 09:17:53 +0300

    виправлення throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,7 +6,7 @@ const player = new Player('vimeo-player');
 player.on('play', function () {
   console.log('played the video!');
 });
-player.on('timeupdate', throttle(setTimeUpdate, 10000));
+player.on('timeupdate', throttle(setTimeUpdate, 1000));
 
 let parseSeconds;
 
