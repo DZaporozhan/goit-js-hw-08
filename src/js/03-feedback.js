ba93abdd6988b90826875825ae1e299a559db786
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-08-16 17:51:54 +0300

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,27 +11,26 @@ refs.form.addEventListener('input', onInputValue);
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  console.log({
-    email: refs.input.value,
-    message: refs.textarea.value,
-  });
+  console.log(formData);
   evt.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
 }
 let formData = {};
 
 function onInputValue(evt) {
+  formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+
   formData[evt.target.name] = evt.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-  formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
 }
 
-const formValue = JSON.parse(localStorage.getItem('feedback-form-state'));
 function inputSaveFormData() {
+  const formValue = JSON.parse(localStorage.getItem('feedback-form-state'));
   if (!formValue) {
     return;
+  } else {
+    refs.input.value = formValue.email || '';
+    refs.textarea.value = formValue.message || '';
   }
-  refs.input.value = formValue.email ? formValue.email : '';
-  refs.textarea.value = formValue.message ? formValue.message : '';
 }
 inputSaveFormData();
